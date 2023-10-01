4bee56caace768dbcbee6b8b7071ab85db474da2
Thanh Trong <doicodon03@gmail.com>
2023-10-01 21:43:48 +0700

    Chỉnh sửa các đường dẫn api công việc

--- back-end/app/routes/task.routes.js
+++ back-end/app/routes/task.routes.js
@@ -7,6 +7,7 @@ module.exports = (app) => {
   router.get("/", tasks.get_CongViec);
   router.get("/nv", tasks.get_CongViec_Nv);
   router.get("/created", tasks.get_created_tasks);
+  router.get("/taskinfo", tasks.getTaskById);
   router.get("/:id", tasks.get_CV_KeHoach);
   router.post("/", tasks.them_CongViec);
   router.post("/updateStatus", tasks.updateTaskStatus);
